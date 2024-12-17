4a90ca3db94bf2349f74c1c20eca0c585de6c2ba
Mo6x <christmoses1999@icloud.com>
2024-12-17 08:23:23 +0100

    working on it

--- src/Components/LandingPagesLayouts/HeroLayouts/HeroLayouts.tsx
+++ src/Components/LandingPagesLayouts/HeroLayouts/HeroLayouts.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-// import Shapes from "../../../assets/Shape.svg";
+import Shapes from "../../../assets/Shape.svg";
 import Navigate1 from "../../../assets/Navigate1.svg";
 import Navigate2 from "../../../assets/Navigate2.svg";
 import Navigate3 from "../../../assets/Navigate3.svg";
@@ -21,25 +21,28 @@ export const HeroLayouts: React.FC = () => {
                     <div className={Styles.HeroLayoutsLists1}>
                         <h1 className={Styles.HeroLayoutH1}>DO YOU NEED</h1>
                     </div>
-                    <div className={Styles.HeroLayoutsListItems}>
-                        <img
-                            src={Navigate1}
-                            alt="Navigate to IndetifyDesign"
-                            className={Styles.HeroLayoutImage}
-                            onClick={() => handleNavigation("/IndetifyDesign")}
-                        />
-                        <img
-                            src={Navigate2}
-                            alt="Navigate to ContentDevelopemts"
-                            className={Styles.HeroLayoutImage}
-                            onClick={() => handleNavigation("/ContentDevelopemts")}
-                        />
-                        <img
-                            src={Navigate3}
-                            alt="Navigate to WebDevelopements"
-                            className={Styles.HeroLayoutImage}
-                            onClick={() => handleNavigation("/WebDevelopements")}
-                        />
+                    <div className={Styles.HeaderLayoutsImageConatiner}>
+                        <img src={Shapes} alt="sharp images" className={Styles.HeroLayoutSharpImage} />
+                        <div className={Styles.HeroLayoutsListItems}>
+                            <img
+                                src={Navigate1}
+                                alt="Navigate to IndetifyDesign"
+                                className={Styles.HeroLayoutImage}
+                                onClick={() => handleNavigation("/IndetifyDesign")}
+                            />
+                            <img
+                                src={Navigate2}
+                                alt="Navigate to ContentDevelopemts"
+                                className={Styles.HeroLayoutImage}
+                                onClick={() => handleNavigation("/ContentDevelopemts")}
+                            />
+                            <img
+                                src={Navigate3}
+                                alt="Navigate to WebDevelopements"
+                                className={Styles.HeroLayoutImage}
+                                onClick={() => handleNavigation("/WebDevelopements")}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
